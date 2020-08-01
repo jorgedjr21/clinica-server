ec5a78400c59b6833dfb724ae629d6f23d405c17
Jorge Junior <jorgedjr21@gmail.com>
2020-08-01 00:12:49 -0300

    config cors

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,5 @@ module OdontoclinServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
   end
 end
